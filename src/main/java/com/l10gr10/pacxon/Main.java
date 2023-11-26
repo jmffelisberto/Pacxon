47933a256442812b23a42aad6e0b1f7569646347
José Felisberto <jm.ffelisberto@gmail.com>
2023-11-26 00:14:48 +0000

    Refactor on structure

--- src/main/java/com/l10gr10/pacxon/Main.java
+++ src/main/java/com/l10gr10/pacxon/Main.java
@@ -1,4 +1,4 @@
-package com.jmffelisberto.hero;
+package com.l10gr10.pacxon;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
