b06ee3f9b7a59dbcbfcaa74b495f36aa528ab331
José Felisberto <jm.ffelisberto@gmail.com>
2023-12-10 17:54:58 +0000

    Finished Rules for now

--- src/main/java/com/l10gr10/pacxon/view/rules/RulesViewer.java
+++ src/main/java/com/l10gr10/pacxon/view/rules/RulesViewer.java
@@ -14,12 +14,25 @@ public class RulesViewer extends Viewer<Rules> {
 
     @Override
     protected void drawElements(GUI gui) throws IOException {
-        gui.drawText(new Position(10, 5), "Rules", "Blue");
+        gui.drawText(new Position(15, 3), "Rules", "Blue");
 
-        gui.drawText(new Position(2, 7), "P-PacXon, Under your control!", "#FFFFFF");
-        gui.drawText(new Position(2, 9), "M-Monsters, They're chasing you!", "#FFFFFF");
-        gui.drawText(new Position(2, 11), "B-Beers, Stop monsters for 5secs.", "#FFFFFF");
-        gui.drawText(new Position(2, 13), "H-Hearts, 1UP to the PacXon!", "#FFFFFF");
+
+        gui.drawText(new Position(1, 7), "Conquer spaces, capture monsters!", "#FFFFFF");
+        gui.drawText(new Position(1, 9), "They can't go through walls, but ", "#FFFFFF");
+        gui.drawText(new Position(0, 10), "if they catch you, you lose 1 life.", "#FFFFFF");
+
+
+        gui.drawText(new Position(1, 12), "As soon as you fill 80% of the ", "#FFFFFF");
+        gui.drawText(new Position(0, 13), "board, you win!", "#FFFFFF");
+
+        gui.drawText(new Position(2, 16), "P-PacXon, Under your control!", "#FFFFFF");
+        gui.drawText(new Position(2, 18), "M-Monsters, They're chasing you!", "#FFFFFF");
+        gui.drawText(new Position(2, 20), "B-Beers, Stop monsters for 5secs.", "#FFFFFF");
+        gui.drawText(new Position(2, 22), "H-Hearts, 1UP to the PacXon!", "#FFFFFF");
+
+        gui.drawText(new Position(7, 29), "Enter - Back to Menu", "#FFD700");
     }
+
+
 
 }
