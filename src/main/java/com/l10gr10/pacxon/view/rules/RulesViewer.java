64ef6cb31efa76056ca75539a87967a949c6df76
José Felisberto <jm.ffelisberto@gmail.com>
2023-12-10 17:36:36 +0000

    Added some Info to Rules

--- src/main/java/com/l10gr10/pacxon/view/rules/RulesViewer.java
+++ src/main/java/com/l10gr10/pacxon/view/rules/RulesViewer.java
@@ -15,5 +15,11 @@ public class RulesViewer extends Viewer<Rules> {
     @Override
     protected void drawElements(GUI gui) throws IOException {
         gui.drawText(new Position(10, 5), "Rules", "Blue");
+
+        gui.drawText(new Position(2, 7), "P-PacXon, Under your control!", "#FFFFFF");
+        gui.drawText(new Position(2, 9), "M-Monsters, They're chasing you!", "#FFFFFF");
+        gui.drawText(new Position(2, 11), "B-Beers, Stop monsters for 5secs.", "#FFFFFF");
+        gui.drawText(new Position(2, 13), "H-Hearts, 1UP to the PacXon!", "#FFFFFF");
     }
+
 }
